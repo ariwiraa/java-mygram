04829174ab6347d655f8093e809cb9a3119597d8
unknown <ariwiraatmaja@gmail.com>
2023-08-18 11:25:05 +0700

    add created_at and updated_at

--- src/main/java/com/example/mygram/model/Photo.java
+++ src/main/java/com/example/mygram/model/Photo.java
@@ -1,6 +1,10 @@
 package com.example.mygram.model;
 
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
 public class Photo {
@@ -8,9 +12,27 @@ public class Photo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
 
-
     private String description;
     private String image;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+//    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = new Date();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = new Date();
+    }
+
 
     public Integer getId() {
         return Id;
@@ -34,5 +56,21 @@ public class Photo {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
